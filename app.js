af4db32273f640ea1a9695fb16fbc0132bcaf6da
SebastianK2000 <sebastian.kosciolek2000@gmail.com>
2023-11-09 20:57:04 +0100

    update and add js

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let button = document.getElementById('first'); // Użyj getElementById z ID przycisku "first"
+    
+    button.addEventListener('click', function() {
+      alert('Sent Successfully! Thank you for your feedback! :)');
+    });
+  });
+  
