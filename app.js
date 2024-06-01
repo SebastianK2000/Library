fde0cddadd86702dc0720ecac9d8d16c40263b18
SebastianK2000 <sebastian.kosciolek2000@gmail.com>
2024-06-01 14:44:34 +0200

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let button = document.getElementById('first'); // Użyj getElementById z ID przycisku "first"
+    let button = document.getElementById('first');
     
     button.addEventListener('click', function() {
       alert('Sent Successfully! Thank you for your feedback! :)');
